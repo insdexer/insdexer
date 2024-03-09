5804f5f81a3d1b6827d4806521e87cb2de1aaef5
bodhidust <bodhi@gmail.com>
2024-03-10 00:51:37 +0800

    update api

--- src/api/router_other.rs
+++ src/api/router_other.rs
@@ -24,22 +24,22 @@ async fn status(state: WebData) -> impl Responder {
     let top_sync_id = db.get_top_inscription_sync_id();
     let sync_blocknumber = db.get_sync_blocknumber();
 
-    match db.get_inscription_by_id(top_insc_id) {
-        Some(insc) => {
-            let current_blocknumber = *state.blocknumber.read().unwrap();
-            let insc_blocknumber = insc.blocknumber;
-            let result = json!({
-                "latest_id": top_insc_id,
-                "latest_sync_id": top_sync_id,
-                "block_number": current_blocknumber,
-                "block_number_inscribe": insc_blocknumber,
-                "block_number_sync": sync_blocknumber,
-                "block_number_behind": insc_blocknumber as i64 - current_blocknumber as i64,
-            });
-            HttpResponse::response_data(result)
-        }
-        None => HttpResponse::response_error_notfound(),
-    }
+    let current_blocknumber = *state.blocknumber.read().unwrap();
+    let insc_blocknumber = match db.get_inscription_by_id(top_insc_id) {
+        Some(insc) => insc.blocknumber,
+        None => 0,
+    };
+
+    let result = json!({
+        "latest_id": top_insc_id,
+        "latest_sync_id": top_sync_id,
+        "block_number": current_blocknumber,
+        "block_number_inscribe": insc_blocknumber,
+        "block_number_sync": sync_blocknumber,
+        "block_number_behind": insc_blocknumber as i64 - current_blocknumber as i64,
+    });
+
+    HttpResponse::response_data(result)
 }
 
 #[get("/checkpoints")]
